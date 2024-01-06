f32f2ee543ce2dbf158f77809a006a24189ec3b6
jp <jp@jps-MacBook-Air.local>
2024-01-06 01:22:52 -0300

    added meta tag

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Add your Google site verification meta tag here */}
+        <meta
+          name="google-site-verification"
+          content="l10zz5xHZ-8fGVFaKPYKh17XweK64UA0jf2XwGsw194"
+        />
+        {/* Other meta tags, like for fonts or favicons, can also be included here */}
+      </Head>
       <body>
         <Main />
         <NextScript />
